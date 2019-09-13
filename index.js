ae8c4dd9a8725764384f23863450f210d3203e6a
michelleurrea <michelleurrea@gmail.com>
2019-09-13 14:26:43 -0700

    Rendered data for a five day forecast

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var request = require('request');
+var geocoder = require('simple-geocoder');
+var moment = require('moment');
+require('dotenv').config()
 
 // Declare your app
 var app = express();
@@ -16,11 +20,46 @@ app.use(parser.urlencoded({ extended: false }));
 
 // Declare routes
 app.get('/', function(req, res){
-  res.render('home');
+	res.render('home');
 });
 
+let icons = {
+  "partly-cloudy-day": '☀️',
+  "cloudy": '☁️',
+  "partly-cloudy-day": '🌥',
+  "rain": '🌧'
+}
+
+
 app.post('/', function(req, res){
-  res.render('result');
+	console.log(req.body);
+	geocoder.geocode(req.body.location, function(success, locations) {
+		let lat = locations.y;
+		let long = locations.x;
+		let requestUrl = process.env.DARK_SKY_BASE_URL + lat + ',' + long;
+		console.log("Request URL: " + requestUrl)
+		if(success) {
+			console.log("Location: ", lat, long);
+			request(requestUrl, (err, response, body) => {
+				if (err || response.statusCode != 200) {
+					console.log(err, response, body)
+					res.render('404');
+				} else {
+					let locationInfo = JSON.parse(body)
+                    console.log(locationInfo)
+                    res.render('result', {
+                        location: req.body.location, 
+                        longitude: long.toFixed(2), 
+                        latitude: lat.toFixed(2),
+                        moment: moment,
+                        icons: icons,
+                        temp: locationInfo.currently,
+                        data: locationInfo.daily.data
+                    })
+				}
+			})
+		}
+	});
 });
 
 // Listen on PORT 3000
